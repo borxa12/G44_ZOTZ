c440d2c8d83a2f7f142e99789e676f36c1bdc9d5
nidesop94 <nidesop94@hotmail.es>
2015-11-23 20:31:04 +0100

    aceptardenegar finalizado

--- view/organizador/aceptardenegarpropuesta.php
+++ view/organizador/aceptardenegarpropuesta.php
@@ -10,25 +10,37 @@
     if($_SESSION['tipo'] != 'org') {
         header("Location: http://localhost/Zotz/index.php");
     } else {
+    	$id = $_GET["idpincho"];
+	$datos = datosPropuestaGastronomica($id);
+	$fila = mysqli_fetch_assoc($datos);
 ?>
+<div>
 				<h1>Propuestas Gastronómicas</h1>
-                <h4> Nombre Pincho : </h4>
-                <h4> Ingredientes : </h4>
-                <h4> Descripcion : </h4>
-                <div class="">
-                    <h4> Foto: </h4>
-                    <a href="#"><img src="#" alt="image" class="img"/></a>
-                </div>
-                <div class="button">
-                    <form id="evaluacionpincho" method="post">
-                        <button type="submit" formaction="propuestasgastronomicas.php" class="btn btn-default button">Aceptar</button>
-                        <button type="submit" formaction="propuestasgastronomicas.php" class="btn btn-default button">Denegar</button>
-                    </form>
-                </div>
-    </div>
+				<?php
+				echo "<h4>Nombre: ".$fila["nombrepincho"]."</h4>";
+				echo "<h4>Ingredientes: ".$fila["ingredientesp"]."</h4>";
+				echo "<h4>Descripcion: ".$fila["descripcionpincho"]."</h4>";
+				echo "<h4>Precio: ".$fila["precio"]."</h4>";
+				
+				?>   
+	<div class="">
+	<h4> Foto: </h4>
+	<a href="#"><img src="#" alt="image" class="img"/></a>
+	</div>
+	<div class="button">
+		<form id="aceptarpincho" method="POST">
+		<input type="hidden" name="idpincho" value="<?php echo $id?>">
+		<button  type="submit" formaction="propuestasgastronomicas.php" name="Aceptar" class="btn btn-default button">Aceptar</button> 
+		<button type="submit" formaction="propuestasgastronomicas.php" name="Denegar" class="btn btn-default button">Denegar</button>
+		</form>
+					
+	</div>
+	</div>
 </div>
 <div class="col-xs-12 col-sm-12 col-md-1"></div>
 </div>
+			
+
 
 <?php
     }
