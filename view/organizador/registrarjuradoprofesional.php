ec96b5eaa1bd28b42cdc267185a5a6da0c85d648
nidesop94 <nidesop94@hotmail.es>
2015-11-24 20:01:10 +0100

    Update registrarjuradoprofesional.php

--- view/organizador/registrarjuradoprofesional.php
+++ view/organizador/registrarjuradoprofesional.php
@@ -31,7 +31,7 @@
                         </div>
                         <div>
                             <label for="passwordjuradoprofesional">Password</label>
-                            <input title="Longitud máxima : 20" type="password" id="passwordjuradoprofesional" pattern="![ñ`´]" maxlength=20 required/>
+                            <input title="No se admiten Ñ ni acentos" type="password" id="passwordjuradoprofesional" pattern="![Ññ`´]" maxlength=20 required/>
                             <br></br>
                         </div>
                         <div>
