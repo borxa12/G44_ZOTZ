7fe213044ff3e678d5c28638ec53b1c1660556f7
csnunhez2 <csnunhez2@esei.uvigo.es>
2015-11-25 13:50:25 +0100

    Update datospincho.php

--- view/establecimiento/datospincho.php
+++ view/establecimiento/datospincho.php
@@ -1,36 +1,41 @@
 <?php
     session_start();
     ob_start();
+    ini_set("display_errors",1);
     include("../../loader.php");
     loadclasses("view","header.php");
     loadclasses("menus","menuestablecimiento.html");
+    loadclasses("controller","ControladorEstablecimiento.php");
     //require_once '../header.php';
     //require_once '../../menus/nomenu.html';
 
     if($_SESSION['tipo'] != 'est') {
         header("Location: http://localhost/Zotz/index.php");
     } else {
+      $id = $_GET['idpincho'];
+      $res = recuperarDatosPincho($id);
+      $pincho = mysqli_fetch_assoc($res);
 ?>
 		<form id="registropincho" method="post">
             <div id=templatemo_form>
                 <div>
                     <label for="nombrepincho">Nombre</label>
-                    <h4>Nombre Pincho</h4>
+                    <h4><?php echo $pincho['nombrepincho'];?></h4>
                     <br></br>
                 </div>
                 <div>
                     <label for="descripcionpincho">Descripcion</label>
-                    <h4>Descripcion Pincho</h4>
+                    <h4><?php echo $pincho['descripcionpincho'];?></h4>
                     <br></br>
                 </div>
                 <div>
                     <label for="ingrdientespincho">Ingredientes</label>
-                    <h4>Ingredientes Pincho</h4>
+                    <h4><?php echo $pincho['ingredientesp'];?></h4>
                     <br></br>
                 </div>
                 <div>
                     <label for="fotopincho">Foto</label>
-                    <img src="" alt="image" class="img">
+                    <?php echo "<img src='../../img/pinchos/".$pincho['fotopincho']."' alt='image' class='img'>"; ?>
                     <br></br>
                 </div>
 		    </div>
