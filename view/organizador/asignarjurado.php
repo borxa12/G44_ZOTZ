010aeb438ba48399ac2f62cc18ff723e627af2e1
csnunhez2 <csnunhez2@esei.uvigo.es>
2015-11-25 20:11:20 +0100

    Update asignarjurado.php

--- view/organizador/asignarjurado.php
+++ view/organizador/asignarjurado.php
@@ -1,49 +1,73 @@
 <?php
     session_start();
     ob_start();
+    ini_set('display_errors',1);
     include("../../loader.php");
     loadclasses("view","header.php");
     loadclasses("menus","menuorganizador.html");
+    loadclasses("controller","ControladorOrganizador.php");
     //require_once '../header.php';
     //require_once '../../menus/nomenu.html';
 
     if($_SESSION['tipo'] != 'org') {
         header("Location: http://localhost/Zotz/index.php");
     } else {
+
+      $id = $_GET['idpincho'];
+      $datos = datosPropuestaGastronomica($id);
+      $pincho = mysqli_fetch_assoc($datos);
+
+      if(isset($_POST['guardar'])){
+        if(isset($_POST['jurado'])){
+        $jurados = $_POST['jurado'];
+        asignarPinchosJuradoProfesional($id,$jurados);
+        }
+        header("Location: http://localhost/Zotz/view/organizador/asignarpinchojuradoprofesional.php");
+}
+
 ?>
 				<form id="registropincho" method="post">
                     <div id=templatemo_form>
                         <div>
                             <label for="nombrepincho">Nombre</label>
-                            <h4>Nombre Pincho</h4>
+                            <h4><?php echo $pincho['nombrepincho'];?></h4>
                             </br>
                         </div>
                         <div>
                             <label for="descripcionpincho">Descripcion</label>
-                            <p>Descripcion Pincho</p>
+                            <p><?php echo $pincho['descripcionpincho'];?></p>
                             </br>
                         </div>
                         <div>
                             <label for="ingrdientespincho">Ingredientes</label>
-                            <p>Ingredientes Pincho</p>
+                            <p><?php echo $pincho['ingredientesp'];?></p>
+                            </br>
+                        </div>
+                        <div>
+                            <label for="ingrdientespincho">Ingredientes</label>
+                            <p><?php echo $pincho['precio'];?></p>
                             </br>
                         </div>
                         <div>
                             <label for="fotopincho">Foto</label>
-                            <img src="" class="img" alt="image">
+                            <img src="../../img/pinchos/<?php echo $pincho['fotopincho'];?>" class="img" alt="image">
                             </br>
                         </div>
 						<div>
 							<label for="jurado">Miembros del jurado</label></br></br>
-							<input type="checkbox" name="jurado1" value="jurado1">jurado1<br>
-							<input type="checkbox" name="jurado2" value="jurado2">jurado2<br>
-							<input type="checkbox" name="jurado3" value="jurado3">jurado3<br>
-							<input type="checkbox" name="jurado4" value="jurado4">jurado4<br>
-							<input type="checkbox" name="jurado5" value="jurado5">jurado5<br><br><br>
+              <?php
+                $listajurado = listarJuradoProfesionalNoAsignado($id);
+                if($listajurado!=null){
+                  while($linea = mysqli_fetch_assoc($listajurado)){
+                  echo "<input type='checkbox' name='jurado[]' value='".$linea['usuarios_login']."'>".$linea['nombrejuradoprofesional']."<br>";
+                }
+              }
+              ?>
 						</div>
 				    </div>
-					<button type="submit" formaction="asignarpinchojuradoprofesional.php" class="btn btn-default button">Guardar</button>
+					<button name="guardar" type="submit" class="btn btn-default button">Guardar</button>
                 </form>
+
     </div>
 </div>
 <div class="col-xs-12 col-sm-12 col-md-1"></div>
