a6c4e62d61014719b8ffac717c5f2fe92af9591c
csnunhez2 <csnunhez2@esei.uvigo.es>
2015-11-25 18:45:26 +0100

    Update modificarestablecimiento.php

--- view/establecimiento/modificarestablecimiento.php
+++ view/establecimiento/modificarestablecimiento.php
@@ -1,15 +1,18 @@
 <?php
     session_start();
     ob_start();
+    ini_set('display_errors',1);
     include("../../loader.php");
     loadclasses("view","header.php");
     loadclasses("menus","menuestablecimiento.html");
+    loadclasses("controller","ControladorEstablecimiento.php");
     //require_once '../header.php';
     //require_once '../../menus/nomenu.html';
 
     if($_SESSION['tipo'] != 'est') {
         header("Location: http://localhost/Zotz/index.php");
     } else {
+      $establecimiento = recuperarDatosEstablecimiento($_SESSION['login']);
 ?>
 				<h1>Datos Establecimiento</h1>
                 <div class="cleaner_h40"></div>
@@ -17,49 +20,58 @@
                     <div id=templatemo_form>
                         <div>
                             <label for="nombreestablecimiento">Nombre: </label>
-							<h5>Nombre Establecimiento</h5>
+							<h5><?php echo $establecimiento['nombre'];?></h5>
                             </br>
                         </div>
                         <div>
                             <label for="direccionestablecimiento">Dirección</label>
-                            <h4>Direccion Establecimiento</h4>
+                            <h4><?php echo $establecimiento['direccion'];?></h4>
                             </br>
                         </div>
                         <div>
                             <label for="telefonoestablecimiento">Teléfono</label>
-                            <h4>Teléfono Establecimiento</h4>
+                            <h4><?php echo $establecimiento['telefono'];?></h4>
                             </br>
                         </div>
                         <div>
                             <label for="webestablecimiento">Web</label>
-                            <h4>Web Establecimiento</h4>
+                            <h4><?php echo $establecimiento['web'];?></h4>
                             </br>
                         </div>
                         <div>
                             <label  for="loginestablecimiento">Login</label>
-                            <h4>Login Establecimiento</h4>
+                            <h4><?php echo $establecimiento['login'];?></h4>
                             </br>
                         </div>
                         <div>
                             <label for="emailestablecimiento">Email</label>
-                            <h4>Email Establecimiento</h4>
+                            <h4><?php echo $establecimiento['email'];?></h4>
                             </br>
                         </div>
                         <div>
                             <label  for="horarioestablecimiento">Horario</label>
-                            <h4>Horario Establecimiento</h4>
+                            <h4><?php echo $establecimiento['horario'];?></h4>
                             </br>
                         </div>
                         <div>
                             <label for="descripcionestablecimiento">Descripción</label>
-                            <h4>Descripción Establecimiento<h4>
+                            <h4><?php echo $establecimiento['descripcion'];?><h4>
                             </br>
                         </div>
                     </div>
                     <form id="actionestablecimientodeletemodify" method="post">
+                       <input name="concurso" hidden="true" value="<?php echo $establecimiento['login'];?>"type = "text"/>
                         <button type="submit" formaction="cambiardatosestablecimiento.php" class="btn btn-default button">Modificar</button>
-                        <button type="button" class="btn btn-default button" data-toggle="modal" data-target="#eliminarestablecimiento">Eliminar</button>
+                        <button type="button" onclick="confirmar()" class="btn btn-default button" data-toggle="modal" data-target="#eliminarestablecimiento">Eliminar</button>
                     </form>
+                    <script>
+                    function confirmar() {
+                        var r = confirm("¿Esta seguro de eliminar el establecimiento?");
+                        if (r == true) {
+                            window.location="index.php?eliminar=true";
+                        }
+                      }
+                      </script>
 
                 <div class="cleaner"></div>
 </div>
