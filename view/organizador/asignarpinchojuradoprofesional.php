69c4f324c7460c814cff47c5e4483899432ee6ca
csnunhez2 <csnunhez2@esei.uvigo.es>
2015-11-25 20:06:54 +0100

    Update asignarpinchojuradoprofesional.php

--- view/organizador/asignarpinchojuradoprofesional.php
+++ view/organizador/asignarpinchojuradoprofesional.php
@@ -1,27 +1,36 @@
 <?php
     session_start();
     ob_start();
+    ini_set('display_errors',1);
     include("../../loader.php");
     loadclasses("view","header.php");
     loadclasses("menus","menuorganizador.html");
+    loadclasses("controller","ControladorOrganizador.php");
     //require_once '../header.php';
     //require_once '../../menus/nomenu.html';
 
     if($_SESSION['tipo'] != 'org') {
         header("Location: http://localhost/Zotz/index.php");
     } else {
+      $lista = listarPinchosAceptados();
 ?>
 				 <h1>Pinchos</h1>
 
-                <div class="product_box">
-                    <a href=""  ><img src="" alt="image" class="img" /></a>
-                    <h4>Nombre Pincho</h4>
-                    <p> Descripción </p>
-                    <form id="asinaciondejurado" method="post">
-                        <button type="submit" formaction="asignarjurado.php" class="btn btn-default button">Asignar Jurado</button>
-                    </form>
-                </div>
-            </div>
+          <?php
+               if($lista){
+         		while ($fila = mysqli_fetch_assoc($lista)) {
+         			echo "<div class='product_box'>";
+         			echo "<a href='asignarjurado.php?idpincho=".$fila['idpincho']."' class='pirobox'><img src='' alt='image' class='img'/></a>";
+         			echo "<h4>".$fila["nombrepincho"]."</h4>";
+         			echo "<p>".$fila["descripcionpincho"]."</p>";
+              echo "<form id='asignarpincho' method='post'>";
+         			echo "<button type='submit' formaction='asignarjurado.php?idpincho=".$fila['idpincho']."' class='btn btn-default button'>Asignar</button>";
+         	        echo "</div>";
+              echo "</form>";
+         		}
+         	}
+
+           ?>
 </div>
 <div class="col-xs-12 col-sm-12 col-md-1"></div>
 </div>
