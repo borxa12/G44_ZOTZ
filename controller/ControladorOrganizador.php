2b6f54818c427b0d3a47a06be44b2596ae82f8c3
csnunhez2 <csnunhez2@esei.uvigo.es>
2015-11-25 20:08:37 +0100

    Update ControladorOrganizador.php

--- controller/ControladorOrganizador.php
+++ controller/ControladorOrganizador.php
@@ -7,14 +7,41 @@
 	loadclasses("model","Usuarios.php");
 	loadclasses("model","VotaProfesional.php");
 
-/*Método que gestiona una propuesta. Si el organizador la acepta, pone le campo aceptado a 'A' y si la rechaza
-pone el campo aceptado a 'D'. Por defecto este campo está a 'N'.
-Parámetros:
-	$id: es la id del pincho que se quiere gestionar.
-	$a: es la opción elegida por el organizador. Si es aceptar esta variable vale 'A' y si es denegar vale 'D'.
-Return: devuelve el pincho con su campo aceptado modificado.
-*/
-   function gestionarPropuesta($id,$a) {
+/*	public function revisarPinchos() {
+
+	}
+
+    public function recuperarPincho() {
+
+    }
+
+    public function seleccionarPropuesta() {
+
+	}
+
+    public function aceptarPropuesta() {
+
+	}
+
+    public function denegarPropuesta() {
+
+	}
+
+    public function registrarPincho() {
+
+    }
+
+    public function eliminarPincho() {
+
+    }
+    */
+
+		function listarJuradoProfesionalNoAsignado($id){
+			$juradoprofesional = new JuradoProfesional();
+			return $juradoprofesional->juradoNoAsignado($id);
+		}
+
+	function gestionarPropuesta($id,$a) {
 		$datos = datosPropuestaGastronomica($id);
 		$fila = mysqli_fetch_assoc($datos);
 		$id = $fila["idpincho"];
@@ -26,37 +53,25 @@ Return: devuelve el pincho con su campo aceptado modificado.
 		$pincho = new Pincho();
 		return $pincho->modificar($id, $nombre, $foto, $descripcion, $ingredientes, $precio, $a);
 	}
-	
-	/*Función que lista aquellos pinchos que estean sin gestionar, es decir que tengan el campo aceptado a N. 
-	Son todos aquellos pinchos que el organizador tiene que revisar para aceptar o denegar. 
-	Sin parámetros. 
-	Return: devuelve un array con los pinchos sin gestionar. 
-	*/	
 
-   function listarPinchosSinAceptar(){
+	function listarPinchosSinAceptar(){
 		$pincho = new Pincho();
 		$lista = $pincho->listarSinGestionar();
 		return $lista;
-   }
-   
- 
-	/*  Recupera los finalistas de la BD, es decir, los pinchos con mejor media de las notas del jurado profesional. 
-	Parametros: 
-	$num - Número máximo de finalistas. 
-	Return: Devuelve un conjunto de datos y en caso contrario FALSE. */
+	}
 
+
+		function listarPinchosAceptados(){
+			$pincho = new Pincho();
+			$lista = $pincho->listarAceptados();
+			return $lista;
+		}
 
     function seleccionarFinalistas($num) {
 		$pincho = new VotaProfesional();
 		$res = $pincho->eleccionFinalistas($num);
 		return $res;
 	}
-	
-	/*Comprueba si el número de participantes es mayor que el número de pinchos finalistas.
-	Parámetros.
-		$num: es el número de pincho seleccionados como finalistas.
-	Return: TRUE si es mayor y FALSE si no lo es.
-	*/
 
     function comprobarParticipantes($num) {
 		$pincho = new Pincho();
@@ -64,38 +79,47 @@ Return: devuelve el pincho con su campo aceptado modificado.
 		if(mysqli_num_rows($res) >= $num) return true;
 		else return false;
     }
-	    /*Lista el jurado profesional.
-	    Sin parámetros.
-	    Return: Devuelve un array con todos los miembros del jurado profesional. 
-	    */
-	
-   function listarJuradoProfesional(){
-	$jp = new JuradoProfesional();
-	$lista = $jp->listar();
-	return $lista;
-    }
-    
-	    /*Inserta un usuario jurado profesional el la BD. Para ello llama al método insertar() de JuradoProfesional.php y al método 
-	    insertar() de Usuario.php que son los que hacen la inserción directamente en la BD. 
-	    Parámetros:
-	    	$login: es el login de usuario que a su vez coindice con el login de jurado profesional.
-	    	$password: es la contraseña de usuario que a su vez coindice con el login de jurado profesional.
-	    	$email: es el email de usuario que a su vez coindice con el login de jurado profesional.
-	    	$nombre: es el nombre del jurado profesional.
-		$foto: es la foto del jurado profesional.
-		$reconocimientos: son los reconocimientos del jurado profesional.
-		Por defecto el tipo es 'jpro' ya que el usuario es de tipo juradoprofesional.
-	   Return: devuelve el usuario registrado en la tabla usuarios y en la tabla juradoprofesional.
-		    */
 
-   function registrarJuradoProfesional($login,$password,$email,$nombre,$foto,$reconocimientos) {
+    // public function elegirFinalistas() {
+	//
+    // }
 
-	$usuario = new Usuarios();
-	$jpro = new JuradoProfesional();
-	$res1 = $usuario->insertar($login,$password,$email,"jpro");
-	$res2 = $jpro->insertar($login,$foto,$nombre,$reconocimientos);
-	return ($res1 && $res2);
-   }
+    /* public function recuperarJuradoProfesional() {
+
+    }*/
+
+	function registrarJuradoProfesional($login,$password,$email,$nombre,$foto,$reconocimientos) {
+		// $login = $_POST["loginjuradoprofesional"];
+		// $nombre = $_POST["nombrejuradoprofesional"];
+		// $email=$_POST["emailjuradoprofesional"];
+		// $password=$_POST["passwordjuradoprofesional"];
+		// $foto=$_POST["fotojuradoprofesional"];
+		// $reconocimientos=$_POST["reconocimientos"];
+		if ($foto != null) {
+			move_uploaded_file($_FILES['fotojuradoprofesional']['tmp_name'],"../../img/juradoprofesional/".$login.".jpg");
+			$usuario = new Usuario();
+			$jpro = new JuradoProfesional();
+			$res1 = $usuario->insertar($login,$password,$email,"jpro");
+			$res2 = $jpro->insertar($login,$foto,$nombre,$reconocimientos);
+			return ($res1 && $res2);
+		} else {
+			$usuario = new Usuarios();
+			$jpro = new JuradoProfesional();
+			$res1 = $usuario->insertar($login,$password,$email,"jpro");
+			$res2 = $jpro->insertar($login,$foto,$nombre,$reconocimientos);
+			return ($res1 && $res2);
+		}
+
+		// $usuario = new Usuarios();
+		// $juradoprofesional = new JuradoProfesional();
+		// $juradoprofesional->usuarios_login=$login;
+		// $juradoprofesional->nombrejuradoprofesional=$nombre;
+		// $juradoprofesional->fotojuradoprofesional=$foto;
+		// $juradoprofesional->recomientos=$reconocimientos;
+		// //$juradoprofesional->fotojuradoprofesional=$foto;
+		// $usuarios->insertar($login, $password, $email, "jpro");
+		// $juradoprofesional->insertar($juradoprofesional);
+	}
 
 
 	/*  Inserta una nueva tupla a la tabla votaprofesional por cada jurado asignado a un pincho
@@ -114,17 +138,11 @@ Return: devuelve el pincho con su campo aceptado modificado.
 		}
 		return $res;
 	}
-	
-	/*Devuelve los datos de un pincho dado su ID. LLama al método recuperar() de la clase Pincho.php que es el que hace
-	el SELECT en la base de datos.
-	Parámetros:
-		$id: es la id del pincho del que se quiere obtener información.
-	Return: devuelve un array con los datos del pincho indicado.
-	*/
 
 	function datosPropuestaGastronomica($id) {
 		$pincho = new Pincho();
 		$datos =  $pincho->recuperar($id);
 		return $datos;
 	}
+// loadclasses("view","footer.html");
 ?>
