78cb55985270e1538d3e6fa9ac50e9d75ad86bde
nidesop94 <nidesop94@hotmail.es>
2015-11-25 16:47:53 +0100

    Update ControladorOrganizador.php

--- controller/ControladorOrganizador.php
+++ controller/ControladorOrganizador.php
@@ -84,8 +84,7 @@
 		// $foto=$_POST["fotojuradoprofesional"];
 		// $reconocimientos=$_POST["reconocimientos"];
 		if ($foto != null) {
-			move_uploaded_file($_FILES['fotojuradoprofesional']['tmp_name'],"../../img/juradoprofesional/".$login.".jpg");
-			$usuario = new Usuario();
+			$usuario = new Usuarios();
 			$jpro = new JuradoProfesional();
 			$res1 = $usuario->insertar($login,$password,$email,"jpro");
 			$res2 = $jpro->insertar($login,$foto,$nombre,$reconocimientos);
