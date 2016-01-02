b8e894b71c8de8243bea3754eaaadf8c413c0356
nidesop94 <nidesop94@hotmail.es>
2016-01-02 19:22:58 +0100

    Update folleto.php

--- G44_ZOTZ/view/noregister/folleto.php
+++ G44_ZOTZ/view/noregister/folleto.php
@@ -26,7 +26,15 @@
 ?>
 
 <h1>Folleto</h1>
-
+<?php
+$concurso = concursoActual();
+$r = mysqli_fetch_assoc($concurso);
+echo "<div>";
+echo "<div class='product_box'>";
+echo "<img onclick='javascript:this.width=720;this.height=438' ondblclick='javascript:this.width=200;this.height=180' src='../../img/folleto/".$r['folleto']."' width='100'</img>";
+echo '<div>';
+echo 'Clic para ampliar';
+?>
 </div>
 </div>
 </div>
