e52cb0388796483659293ddec67fd71843756aa3
nidesop94 <nidesop94@hotmail.es>
2016-01-02 19:21:47 +0100

    Update principal.php

--- G44_ZOTZ/view/noregister/principal.php
+++ G44_ZOTZ/view/noregister/principal.php
@@ -1,9 +1,14 @@
-<!-- Contenido principal de la página -->
-<h1>XXVII Concurso de Tapas de Ourense</h1>
-<h3>Benvidos, un ano máis, a unha nova edición do concurso de pinchos máis famoso do mundo. O concurso de pinchos por excelencia, o que todos se queren parecer, o que todos envexan. Un ano máis, desfrutade de bos pinchos e sede bos xuíces. <br/><br/>E que o pincho este sempre do voso lado.</h3>
-<h2>Data de Inicio: 4/12/2015</h2>
-<h2>Data de Finalización: 7/1/2016</h2>
-</div>
-</div>
-</div>
-<div class="col-xs-12 col-sm-12 col-md-1"></div>
+<!--Contenido de la pagina principal-->
+<?php
+loadclasses("controller","ControladorOrganizador.php");
+
+$concurso = concursoActual();
+$r = mysqli_fetch_assoc($concurso);
+
+echo "<h1>".$r['titulo']."</h1>";
+echo "<h3>".$r['descripcion']."</h3>";
+echo "<h2>"."Fecha inicio ".$r['fechac']."</h2>";
+echo "<h2>"."Fecha fin  ".$r['fechaf']."</h2>";
+
+
+?>
